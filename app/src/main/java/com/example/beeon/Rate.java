a5f420602e98af4e3085509fc292184330aba1b6
hayyara <ergjamshid@gmail.com>
2019-12-15 10:15:22 +0500

    added new features

---
+++ app/src/main/java/com/example/beeon/Rate.java
@@ -0,0 +1,14 @@
+package com.example.beeon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Rate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rate);
+    }
+}
